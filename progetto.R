3800fd6a519f05d4d2b3c2825c68ea997eef465a
vpresta9@gmail.com <vpresta9@gmail.com>
2024-03-17 19:15:14 +0100

    weighted regression model, BP test/ W test

--- progetto.R
+++ progetto.R
@@ -59,9 +59,9 @@ airbnb <- airbnb[airbnb$price != 0, ]
 # creiamo un nuovo dataframe in cui sono presenti solamente gli attributi significativi per la nostra analisi
 airbnb_numerico <- subset(airbnb, select = -c(id, host_id, latitude, longitude, name, host_name, neighbourhood_group, neighbourhood, room_type, last_review))
 
-# combattiamo la trappola delle dummy
-airbnb_dummytrap <- subset(airbnb_numerico, select = -c(NB_manhattan, RM_entire))
-matrice_correlazione <- cor(airbnb_dummytrap)
+# trappola delle dummy
+airbnb_dummy <- subset(airbnb_numerico, select = -c(NB_manhattan, RM_entire))
+matrice_correlazione <- cor(airbnb_dummy)
 corrplot(matrice_correlazione, method = "circle")
 
 # istogramma della variabile dipendente price
@@ -71,7 +71,7 @@ plot(density(airbnb_numerico$price))
 summary(airbnb_numerico$price)
 
 #modello lineare che tiene in considerazione tutti i regressori
-model <- lm(formula = airbnb_dummytrap$price ~ ., data = airbnb_dummytrap)
+model <- lm(formula = airbnb_dummy$price ~ ., data = airbnb_dummy)
 summary(model)
 
 # matrice di correlazione
@@ -79,7 +79,7 @@ matrice_correlazione <- cor(airbnb_numerico)
 corrplot(matrice_correlazione, method = "circle")
 
 # determinante della matrice XtX
-X <- as.matrix(cbind(rep(1, nrow(airbnb_dummytrap)), airbnb_dummytrap$minimum_nights, airbnb_dummytrap$number_of_reviews, airbnb_dummytrap$reviews_per_month, airbnb_dummytrap$calculated_host_listings_count, airbnb_dummytrap$availability_365, airbnb_dummytrap$NB_brooklyn, airbnb_dummytrap$NB_queens, airbnb_dummytrap$NB_statenisland, airbnb_dummytrap$NB_bronx, airbnb_dummytrap$RM_private, airbnb_dummytrap$RM_shared))
+X <- as.matrix(cbind(rep(1, nrow(airbnb_dummy)), airbnb_dummy$minimum_nights, airbnb_dummy$number_of_reviews, airbnb_dummy$reviews_per_month, airbnb_dummy$calculated_host_listings_count, airbnb_dummy$availability_365, airbnb_dummy$NB_brooklyn, airbnb_dummy$NB_queens, airbnb_dummy$NB_statenisland, airbnb_dummy$NB_bronx, airbnb_dummy$RM_private, airbnb_dummy$RM_shared))
 determinante <- det(t(X) %*% X)
 print(determinante)
 
@@ -99,6 +99,8 @@ vif(model)
 toleranceValue <- 1/vif(model)
 print(toleranceValue)
 
+
+set.seed(123)
 #Verifica della presenza dell'eteroschedasticità dal punto di vista grafico
 residui <- residuals(model)
 ordinate_stimate <- fitted(model)
@@ -112,7 +114,7 @@ plot(ordinate_stimate, residui,
 
 # Test di Breusch-Pagan
 res1 <- residuals(model); res12<- res1^2
-modBPtest <- lm(formula = res12~ ., data = airbnb_dummytrap)
+modBPtest <- lm(formula = res12~ ., data = airbnb_dummy)
 summary(modBPtest)
 
 #Test di White
@@ -121,9 +123,68 @@ modWtest <- lm(res12~fit1+fit12)
 summary(modWtest)
 
 # Alternativa: Trasformazione logaritmica alla variabile dipendente
-model_log_lin <- lm(formula = log(airbnb_dummytrap$price) ~ ., data = airbnb_dummytrap)
+model_log_lin <- lm(formula = log(airbnb_dummy$price) ~ ., data = airbnb_dummy)
+#Nota: Con la trasf logaritmica alla variabile dipendente il modello migliora in termini di R squared e adj- r squared
 
-#TODO: applicazione modello log-lin e ripetizione bp e w test su quel modello, 
-#se ancora presente eteroschedasticità si deve procedere al modello pesato per le ordinate stimate(i regressori vengono divisi per le ordinate stimate)
-#ripetere bp e w test sul modello pesato ...
+#Verifica della presenza dell'eteroschedasticità dal punto di vista grafico
+residui_log <- residuals(model_log_lin)
+log_fitted <- fitted(model_log_lin)
+
+# Crea il grafico 
+plot(log_fitted, residui_log,
+     xlab = "Fitted",
+     ylab = "Residui",
+     main = "Residui vs Log fitted")
+
+# Test di Breusch-Pagan
+res1 <- residuals(model_log_lin); res12<- res1^2
+modBPtest <- lm(formula = res12~ ., data = airbnb_dummy)
+summary(modBPtest)
+
+#Test di White
+fit1<-fitted(model_log_lin); fit12<-fit1^2
+modWtest <- lm(res12~fit1+fit12)
+summary(modWtest)
+
+#Procedere al modello pesato per le ordinate stimate(i regressori vengono divisi per le ordinate stimate)
+airbnb_w <- airbnb_dummy
+
+# Dividere tutti i regressori per le ordinate stimate
+airbnb_w[-1] <- airbnb_w[-1] / fitted(model)
+
+wModel <- lm(formula = price ~ ., data = airbnb_w)
+summary(wModel)
+
+# Test di Breusch-Pagan per il modello pesato 
+res1 <- residuals(wModel); res12<- res1^2
+modBPtest <- lm(formula = res12~ ., data = airbnb_w)
+summary(modBPtest)
+
+#Test di White per il modello pesato 
+fit1<-fitted(wModel); fit12<-fit1^2
+modWtest <- lm(res12~fit1+fit12)
+summary(modWtest)
+
+#A questo punto bisogna pesare il modello per ogni regressore e cercare di eliminare l'eteroschedasticità
+
+#Modello pesato per il regressore
+wAirbnb_regressor <- airbnb_dummy
+wModel_regressor <- lm(formula = price ~ ., data = wAirbnb_regressor, weights = wAirbnb_regressor$NB_queens)
+summary(modello_pesato_mn)
+
+
+# Test di Breusch-Pagan per il modello pesato 
+res1 <- residuals(wModel_regressor); res12<- res1^2
+modBPtest <- lm(formula = res12~ ., data = wAirbnb_regressor, weights = wAirbnb_regressor$NB_queens)
+summary(modBPtest)
+
+#Test di White per il modello pesato 
+fit1<-fitted(wModel_regressor); fit12<-fit1^2
+modWtest <- lm(res12~fit1+fit12)
+summary(modWtest)
+
+
+
+
+#TODO: 
 #se ancora presente si passa a un modello pesato per i regressori
